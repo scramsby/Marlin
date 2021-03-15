730990b0a3617f41a617acf72acc943aa24d441a
Scott Ramsby <scramsby@microsoft.com>
2021-03-14 17:03:19 -0700

    Tweak Z offset and min pos

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -987,7 +987,7 @@
  *     |    [-]    |
  *     O-- FRONT --+
  */
-#define NOZZLE_TO_PROBE_OFFSET { 20.75, 0, -3.175 }
+#define NOZZLE_TO_PROBE_OFFSET { 20.75, 0, -3.1 }
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
@@ -1127,8 +1127,8 @@
 #define Y_BED_SIZE 220
 
 // Travel limits (mm) after homing, corresponding to endstop positions.
-#define X_MIN_POS -12
-#define Y_MIN_POS -82
+#define X_MIN_POS -6.85
+#define Y_MIN_POS -89.8
 #define Z_MIN_POS 0
 #define X_MAX_POS X_BED_SIZE
 #define Y_MAX_POS Y_BED_SIZE + 10
